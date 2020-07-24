ea90dbd650230c9398d393ed43855f1eb9fde5e0
simonojok19 <simonojok19@gmail.com>
2020-07-24 23:55:53 +0300

    added the text styles

--- App.js
+++ App.js
@@ -3,9 +3,21 @@ import { View, Text, SafeAreaView, StyleSheet } from 'react-native';
 
 const App = () => {
     return (
-        <SafeAreaView>
-            <View style={styles.container}>
+        <SafeAreaView style={ { flex: 1, paddingHorizontal: 10 }}>
+            <View style={[styles.container, styles.pink]}>
                 <Text>Hello, world!</Text>
+            </View>
+            <View style={[ styles.container, styles.cyan]}>
+                <Text>Cyan: #2aa198</Text>
+            </View>
+            <View style={[ styles.container, styles.blue ]}>
+                <Text>Blue: #268bd2</Text>
+            </View>
+            <View style={[ styles.container, styles.magenta ]}>
+                <Text>Magenta: #d33682</Text>
+            </View>
+            <View style={[ styles.container, styles.orange ]}>
+                <Text>Orange: #cb4b16</Text>
             </View>
         </SafeAreaView>
     );
@@ -13,9 +25,30 @@ const App = () => {
 
 const styles = StyleSheet.create({
     container: {
-        backgroundColor: 'teal',
-        paddingVertical: 10,
-        paddingHorizontal: 30
+        alignItems: 'center',
+        justifyContent: 'center',
+        color: 'white',
+        padding: 20,
+        marginVertical: 5
+    },
+    pink: {
+        backgroundColor: 'pink'
+    },
+    cyan: {
+        backgroundColor: '#2aa198'
+    },
+    blue: {
+        backgroundColor: '#268bd2'
+    },
+    magenta: {
+        backgroundColor: '#d33682'
+    },
+    orange: {
+        backgroundColor: '#cb4b16'
+    },
+    boxText: {
+        color: 'white',
+        fontWeight: 'bold'
     }
 });
 
