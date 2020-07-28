58ba05046f93ed5d796826ba0140b3ae03f27c35
simonojok19 <simonojok19@gmail.com>
2020-07-28 21:02:21 +0300

    created stacked nagivator

--- App.js
+++ App.js
@@ -1,6 +1,9 @@
 import 'react-native-gesture-handler';
 import React from 'react';
 import {  NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+
+const Stack = createStackNavigator();
 
 const App = () => {
     return (
